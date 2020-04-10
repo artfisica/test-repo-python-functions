c589cde2b94142e6deb55b17eb33015de8f36b90
mildredarias <mildarias181@gmail.com>
2020-04-10 13:10:50 +0200

    cambio

--- Mildred.py
+++ Mildred.py
@@ -3,10 +3,20 @@ def name():
   print("Mildred Arias")
 
 ##second_function
-def pow_two(pot):
-    while pot % 2 !=0: 
-        print 'the number:',pot,',it is not integer'
-        print 'Please enter a whole number'
-        pot=input('the number is: ') 
-    else:
-        print 'the square of the integer is: ',pot**2
+def pow_two():
+    
+    while True:
+        entero=input('enter a number: ')
+        if type(entero)!=int:
+            print('The number is not an integer, please enter a whole number')
+        else:
+            print('The number is an integer')
+            print'the squared power of the integer is: ',entero**2
+            break
+      
+    
+        
+    
+            
+    
+            
